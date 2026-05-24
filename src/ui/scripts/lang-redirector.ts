28578b8c90fe342281940b4499ba4fc9e40b9879
mirzattar <attar.17ma@gmail.com>
2026-05-24 00:17:56 -0400

    Add experimental 404 page

--- src/ui/scripts/lang-redirector.ts
+++ src/ui/scripts/lang-redirector.ts
@@ -1,8 +1,10 @@
 import { getUserLang } from "@js/i18n"
 
 const currentURL = new URL(window.location.href)
-const hasPath = currentURL.pathname.at(0) !== "/"
 
-window.location.replace(
-	`${currentURL.origin}/${getUserLang()}${hasPath ? currentURL.pathname : ""}`
-)
+if (currentURL.pathname.split("/")[ 1 ]?.length !== 2) {
+	const replacementURL = `${currentURL.origin}/${getUserLang()}${(currentURL.pathname.length > 1) ? currentURL.pathname : ""}`
+
+	console.info("Redirecting to:", replacementURL)
+	window.location.replace(replacementURL)
+}
